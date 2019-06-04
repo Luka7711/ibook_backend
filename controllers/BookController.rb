103145dbacd7cc18e96ef8f35452576c23ae1b18
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-06-04 13:36:38 -0500

    profile template added

--- controllers/BookController.rb
+++ controllers/BookController.rb
@@ -1,14 +1,19 @@
 class BookController < ApplicationController
+
+	# redirect to home page
 	get '/' do
+		@books = Book.all
 		@category = Category.all
 		erb :book_index
 	end
 
+	# redirect to create form
 	get '/new' do
 		@category = Category.all
 		erb :book_new
 	end
 
+	# creating a book
 	post '/unwantedList' do 
 		book = Book.new
 		book.title = params[:title]
@@ -21,5 +26,13 @@ class BookController < ApplicationController
 		book.category_id = params[:category].to_i
 		book.save
 		redirect '/ibook'
+	end
+
+	get '/profile' do
+		# find a user
+		user = User.find_by ({:username => session[:username]})
+		# find a list of unwanted books
+		@unwanted_books = Book.where :user_id => user.id
+		erb :profile_show
 	end
 end	
