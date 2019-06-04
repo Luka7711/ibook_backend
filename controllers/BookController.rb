6c1a7b09969faa3d5ec7afbf3acd1abbce386ddf
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-06-04 12:02:58 -0500

    user able to add a book

--- controllers/BookController.rb
+++ controllers/BookController.rb
@@ -8,4 +8,18 @@ class BookController < ApplicationController
 		@category = Category.all
 		erb :book_new
 	end
+
+	post '/unwantedList' do 
+		book = Book.new
+		book.title = params[:title]
+		book.author = params[:author]
+		book.published_year = params[:year]
+		book.description = params[:description]
+		book.image = params[:image]
+		user = User.find_by ({ :username => session[:username]})
+		book.user_id = user.id
+		book.category_id = params[:category].to_i
+		book.save
+		redirect '/ibook'
+	end
 end	
